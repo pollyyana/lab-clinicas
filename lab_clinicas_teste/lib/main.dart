320ea7e930b78c0ad46a52e7763c284a6941f203
pollyyana <pollyana.medeirosk@gmail.com>
2024-01-31 03:08:54 -0300

    asyncState

--- lab_clinicas_teste/lib/main.dart
+++ lab_clinicas_teste/lib/main.dart
@@ -1,30 +1,34 @@
+import 'package:asyncstate/asyncstate.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
 import 'package:lab_clinicas_core/lab_clinicas_core.dart';
 import 'package:lab_clinicas_teste/homer_controller.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return LabClinicasCoreConfig(title: 'Lab Clinicas Teste', pagesBuilders: [
+      FlutterGetItPageBuilder(
+        page: (_) => const MyHomePage(title: 'oij'),
+        path: '/',
+      )
+    ]);
   }
 }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> with MessageViewMixin {
   int _counter = 0;
   final controller = HomeController();
@@ -34,14 +38,17 @@ class _MyHomePageState extends State<MyHomePage> with MessageViewMixin {
     messageListener(controller);
     super.initState();
   }
- 
+
   void _incrementCounter() {
     // Messages.showError('import do core realizado com sucesso', context);
-    controller.fazerAlgo();
+    // controller.fazerAlgo();
+    Future.delayed(const Duration(seconds: 3)).asyncLoader();
+
     setState(() {
       _counter++;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> with MessageViewMixin {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
