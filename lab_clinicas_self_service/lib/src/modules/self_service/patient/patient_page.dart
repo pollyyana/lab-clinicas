1c850bdf4502de640dc9171db040d1bb0e8fb3f3
pollyyana <pollyana.medeirosk@gmail.com>
2024-02-04 18:47:52 -0300

    conectar formulario

--- lab_clinicas_self_service/lib/src/modules/self_service/patient/patient_page.dart
+++ lab_clinicas_self_service/lib/src/modules/self_service/patient/patient_page.dart
@@ -1,9 +1,26 @@
+import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lab_clinicas_core/lab_clinicas_core.dart';
+import 'package:lab_clinicas_self_service/src/modules/self_service/patient/patient_form_controller.dart';
 import 'package:lab_clinicas_self_service/src/modules/self_service/widget/lab_clinicas_self_service_app_bar.dart';
+import 'package:validatorless/validatorless.dart';
 
-class PatientPage extends StatelessWidget {
+class PatientPage extends StatefulWidget {
   const PatientPage({super.key});
+
+  @override
+  State<PatientPage> createState() => _PatientPageState();
+}
+
+class _PatientPageState extends State<PatientPage> with PatientFormController {
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    disposeForm();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,167 +38,219 @@ class PatientPage extends StatelessWidget {
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(16),
                 border: Border.all(color: LabClinicasTheme.orangeColor)),
-            child: Column(
-              children: [
-                Image.asset('assets/images/check_icon.png'),
-                const SizedBox(
-                  height: 24,
-                ),
-                const Text('Cadastro encontrado',
-                    style: LabClinicasTheme.titleSmallStyle),
-                const SizedBox(
-                  height: 32,
-                ),
-                const Text(
-                  'Confirma os dados do seu cadastro ',
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      color: LabClinicasTheme.blueColor),
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                TextFormField(
-                  decoration:
-                      const InputDecoration(label: Text('Nome Paciente')),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(label: Text('Email')),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  decoration:
-                      const InputDecoration(label: Text('Telefone de contato')),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  decoration:
-                      const InputDecoration(label: Text('Digite seu CPF')),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(label: Text('CEP')),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Flexible(
-                      flex: 3,
-                      child: TextFormField(
-                        decoration:
-                            const InputDecoration(label: Text('Endereço')),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 16,
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: TextFormField(
-                        decoration:
-                            const InputDecoration(label: Text('Número')),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Expanded(
-                      child: TextFormField(
-                        decoration:
-                            const InputDecoration(label: Text('Complemento')),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 16,
-                    ),
-                    Expanded(
-                      child: TextFormField(
-                        decoration:
-                            const InputDecoration(label: Text('Estado')),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Expanded(
-                      child: TextFormField(
-                        decoration:
-                            const InputDecoration(label: Text('Cidade')),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 16,
-                    ),
-                    Expanded(
-                      child: TextFormField(
-                        decoration:
-                            const InputDecoration(label: Text('Bairro')),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(label: Text('Responsável')),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  decoration:
-                      const InputDecoration(label: Text('Responsável Identificação')),
-                ),
-                const SizedBox(
-                  height: 32,
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: SizedBox(
-                        height: 48,
-                          child: OutlinedButton(
-                              onPressed: () {}, child: const Text('EDITAR'))),
-                    ),
-                    const SizedBox(
-                       width: 17,
-                    ),
-                    Expanded(
-                      child: SizedBox(
-                        height: 48,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: const Text('CONTINUAR'),
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  Image.asset('assets/images/check_icon.png'),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  const Text('Cadastro encontrado',
+                      style: LabClinicasTheme.titleSmallStyle),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  const Text(
+                    'Confirma os dados do seu cadastro ',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: LabClinicasTheme.blueColor),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  TextFormField(
+                    controller: nameEC,
+                    validator: Validatorless.required('Nome obrigatorio'),
+                    decoration:
+                        const InputDecoration(label: Text('Nome Paciente')),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: emailEC,
+                    validator: Validatorless.multiple([
+                      Validatorless.required('Email obrigatório'),
+                      Validatorless.email('Email inválido')
+                    ]),
+                    decoration:
+                        const InputDecoration(label: Text('Email')),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: phoneEC,
+                    validator: Validatorless.required('Telefone Obrigatório'),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      TelefoneInputFormatter()
+                    ],
+                    decoration: const InputDecoration(
+                        label: Text('Telefone de contato')),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: documentEC,
+                    validator: Validatorless.required('CPF obrigatório'),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      CpfInputFormatter()
+                    ],
+                    decoration:
+                        const InputDecoration(label: Text('Digite seu CPF')),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: cepEC,
+                    validator: Validatorless.required('CEP obrigatório'),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      CepInputFormatter()
+                    ],
+                    decoration: const InputDecoration(label: Text('CEP')),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Flexible(
+                        flex: 3,
+                        child: TextFormField(
+                          controller: streetEC,
+                          validator:
+                              Validatorless.required('Endereço obrigatório'),
+                          decoration:
+                              const InputDecoration(label: Text('Endereço')),
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ],
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: TextFormField(
+                          controller: numberEC,
+                          validator:
+                              Validatorless.required('Numero obrigatório'),
+                          decoration:
+                              const InputDecoration(label: Text('Número')),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          controller: complementEC,
+                          decoration:
+                              const InputDecoration(label: Text('Complemento')),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          controller: stateEC,
+                          validator:
+                              Validatorless.required('Estado obrigatório'),
+                          decoration:
+                              const InputDecoration(label: Text('Estado')),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          controller: cityEC,
+                          validator:
+                              Validatorless.required('Cidade obrigatória'),
+                          decoration:
+                              const InputDecoration(label: Text('Cidade')),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          controller: districtEC,
+                          validator:
+                              Validatorless.required('Bairro obrigatório'),
+                          decoration:
+                              const InputDecoration(label: Text('Bairro')),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: guardianEC,
+                    decoration:
+                        const InputDecoration(label: Text('Responsável')),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: guardianIdentificationNumberEC,
+                    decoration: const InputDecoration(
+                        label: Text('Responsável Identificação')),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      CpfInputFormatter()
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                            height: 48,
+                            child: OutlinedButton(
+                                onPressed: () {}, child: const Text('EDITAR'))),
+                      ),
+                      const SizedBox(
+                        width: 17,
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          height: 48,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('CONTINUAR'),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
