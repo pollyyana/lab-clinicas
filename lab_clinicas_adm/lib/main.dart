90d8f461e546005c2dee26a46868b96753f00fcc
pollyyana <pollyana.medeirosk@gmail.com>
2024-02-13 02:19:41 -0300

    estrutura login linux

--- lab_clinicas_adm/lib/main.dart
+++ lab_clinicas_adm/lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 import 'package:lab_clinicas_adm/src/bindings/lab_clinicas_application_binding.dart';
+import 'package:lab_clinicas_adm/src/core/pages/login/login_router.dart';
 import 'package:lab_clinicas_adm/src/core/pages/splash_page/splash_page.dart';
 import 'package:lab_clinicas_core/lab_clinicas_core.dart';
 
@@ -26,11 +27,10 @@ class LabClinicasAdm extends StatelessWidget {
       title: 'Lab Clinics ADM',
       binding: LabClinicasApplicationBinding(),
       pagesBuilders: [
-        FlutterGetItPageBuilder(
-          page: (_) => const SplashPage(),
-          path: '/',
-        )
+        FlutterGetItPageBuilder(page: (_) => const SplashPage(), path: '/'),
+        FlutterGetItPageBuilder(page: (_) => const Text('estou na home'), path: '/home'),
       ],
+      pages: const [LoginRouter()],
     );
   }
 }
--- lab_clinicas_adm/lib/src/core/pages/splash_page/splash_page.dart
+++ lab_clinicas_adm/lib/src/core/pages/splash_page/splash_page.dart
@@ -1,14 +1,29 @@
+
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
 
-  const SplashPage({ super.key });
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Splash'),),
-           body: Container(),
-       );
+class _SplashPageState extends State<SplashPage> {
+
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) { 
+      Navigator.of(context).pushReplacementNamed('/login');
+    });
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/logo_vertical.png'),
+      ),
+    );
   }
 }
