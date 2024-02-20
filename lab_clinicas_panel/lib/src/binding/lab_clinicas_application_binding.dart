b37c410afc0df58fe0ed2317ab90c1f8b7e324a1
pollyyana <pollyana.medeirosk@gmail.com>
2024-02-20 00:25:04 -0300

    finalizado core para web

---
+++ lab_clinicas_panel/lib/src/binding/lab_clinicas_application_binding.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:lab_clinicas_core/lab_clinicas_core.dart';
+import 'package:lab_clinicas_panel/src/core/env.dart';
+
+class LabClinicasApplicationBinding extends ApplicationBindings {
+  @override
+  List<Bind<Object>> bindings() => [
+    Bind.lazySingleton<RestClient>((i) => RestClient(Env.backendBaseUrl))
+  ];
+}
